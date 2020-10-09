88434302e69794eb0a0b7d05beb6fe1d5fe96bf0
Luis H. Moreno <luishmcmoreno@gmail.com>
2020-10-09 05:01:56 +0200

    LM-007: wss for security and redirects

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   serverUrl: 'https://machinestream.herokuapp.com/api/v1',
-  websocketUrl: 'ws://machinestream.herokuapp.com/api/v1/events',
+  websocketUrl: 'wss://machinestream.herokuapp.com/api/v1/events',
 };
